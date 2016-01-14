7e2dec0adc62a152595fd8118494dd6df678a213
Mindhunter001 <alexander.ruehl@gmail.com>
2016-01-14 00:14:19 -0800

    day1

---
+++ Ticketshuffle/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :firstname, :lastname)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+end
